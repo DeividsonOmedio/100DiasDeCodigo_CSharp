c5b6b956076bb09217f2bdc3cc605e38abc904f2
Deividson Omedio <deividsonomedio@outlook.com>
2024-04-19 00:09:51 -0300

    Dia_47

---
+++ BlazorAppChatSolution/BlazorAppChat.API/Program.cs
@@ -0,0 +1,36 @@
+using BlazorAppChat.API.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.MapHub<ChatHub>("/chat");
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseCors(builder =>
+{
+    builder.AllowAnyOrigin();
+    builder.AllowAnyMethod();
+    builder.AllowAnyHeader();
+});
+
+app.Run();
